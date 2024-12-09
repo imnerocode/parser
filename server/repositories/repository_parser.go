58e8e89eec912cf60cebcf21dec5370c22649dc2
imnerocode <jesalemalo@icloud.com>
2024-12-09 13:46:37 -0500

    feat(repository_parser.go)
    Add function repository parser model it takes the context, the parser request, and returns a parser response if all is correct or error if not

--- server/repositories/repository_parser.go
+++ server/repositories/repository_parser.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"context"
+
 	pb "github.com/imnerocode/parser/parser"
 	"gorm.io/gorm"
 )
@@ -8,4 +10,14 @@ import (
 type ParserRepository struct {
 	Db *gorm.DB
 	pb.UnimplementedParserServiceServer
+}
+
+func NewParserRepository(db *gorm.DB) *ParserRepository {
+	return &ParserRepository{Db: db}
+}
+
+func (r *ParserRepository) ParserModel(ctx context.Context, rq *pb.ParserRequest) (*pb.ParserResponse, error) {
+	response := &pb.ParserResponse{}
+
+	return response, nil
 }
