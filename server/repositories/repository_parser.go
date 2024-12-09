83034657368aeef0ff68391b812e3e7a968339cb
imnerocode <jesalemalo@icloud.com>
2024-12-09 13:38:02 -0500

    feat(repository_parser.go)
    Add gorm dependencies
    
    Add repository structure
    
    Dependencies wich comes from gorm, is a ORM wich manage the connection with the database
    
    Repository structure is make it with a pointer reference of gorm.BD, and with the service parser wich is serialized with the command protoc wich comes with functions handlers data

--- server/repositories/repository_parser.go
+++ server/repositories/repository_parser.go
@@ -1,1 +1,11 @@
 package repositories
+
+import (
+	pb "github.com/imnerocode/parser/parser"
+	"gorm.io/gorm"
+)
+
+type ParserRepository struct {
+	Db *gorm.DB
+	pb.UnimplementedParserServiceServer
+}
